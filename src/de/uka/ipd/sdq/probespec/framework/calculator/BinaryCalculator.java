c5e9f0da36e5e4f605a601557e38e4d96abd6a9b
pmerkle <merkle@kit.edu>
2010-02-21 23:33:22 +0000

    Refactored ResponseTimeCalculator: Introduced abstract BinaryCalculator
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio.ProbeSpecification/trunk/de.uka.ipd.sdq.probespec.framework@8460 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ src/de/uka/ipd/sdq/probespec/framework/calculator/BinaryCalculator.java
@@ -0,0 +1,94 @@
+package de.uka.ipd.sdq.probespec.framework.calculator;
+
+import java.util.Vector;
+
+import javax.measure.Measure;
+import javax.measure.quantity.Quantity;
+
+import de.uka.ipd.sdq.pipesandfilters.framework.MeasurementMetric;
+import de.uka.ipd.sdq.pipesandfilters.framework.PipeData;
+import de.uka.ipd.sdq.probespec.framework.ProbeSetSample;
+import de.uka.ipd.sdq.probespec.framework.ProbeSetSampleID;
+import de.uka.ipd.sdq.probespec.framework.SampleBlackboard;
+import de.uka.ipd.sdq.probespec.framework.exceptions.CalculatorException;
+
+/**
+ * 
+ * @author Philipp Merkle
+ *
+ */
+public abstract class BinaryCalculator extends Calculator {
+
+	private String startProbeSetID;
+	private String endProbeSetID;
+
+	public BinaryCalculator(SampleBlackboard blackboard,
+			String startProbeSetID, String endProbeSetID) {
+		super(blackboard);
+		this.startProbeSetID = startProbeSetID;
+		this.endProbeSetID = endProbeSetID;
+	}
+
+	abstract protected Vector<Measure<?, ? extends Quantity>> calculate(
+			ProbeSetSample start, ProbeSetSample end)
+			throws CalculatorException;
+
+	@Override
+	abstract protected Vector<MeasurementMetric> getConcreteMeasurementMetrics();
+	
+	/**
+	 * This method is called by the
+	 * {@link #update(java.util.Observable, Object)} method as soon as a new
+	 * ProbeSetSample arrives at the blackboard (Observer Pattern). If
+	 * <code>pss</code> is an end probe set sample, the method tries to get the
+	 * corresponding start ProbeSetSample and invokes the
+	 * {@link #calculate(ProbeSetSample, ProbeSetSample)} method. If
+	 * <code>pss</code> is an start ProbeSetSample, this method will do nothing.
+	 * <p>
+	 * After the calculation the result is passed to the pipes-and-filters
+	 * chain.
+	 * <p>
+	 * Here we make the assumption that the start ProbeSetSample always arrives
+	 * before the end ProbeSetSample. Without this assumption all binary
+	 * calculators would also have to try to get the end ProbeSetSample when the
+	 * start ProbeSetSample arrives. Probably this would have a negative effect
+	 * on the performance.
+	 * 
+	 * @param pss
+	 *            the last ProbeSetSample which was added to the
+	 *            SampleBlackboard and so triggered this Calculator.
+	 */
+	@Override
+	protected void execute(ProbeSetSample pss) throws CalculatorException {
+		/*
+		 * Execute only if second ProbeSetSample arrives. Here we make the
+		 * assumption that the start ProbeSetSample always arrives before the
+		 * end ProbeSetSample. See JavaDoc comment above.
+		 */
+		if (endProbeSetID.equals(pss.getProbeSetSampleID().getProbeSetID())) {
+			ProbeSetSample endSetSample = pss;
+			ProbeSetSample startSetSample = blackboard
+					.getProbeSetSample(new ProbeSetSampleID(startProbeSetID,
+							pss.getProbeSetSampleID().getCtxID()));
+			if (startSetSample != null) {
+				Vector<Measure<?, ? extends Quantity>> resultTuple = calculate(
+						startSetSample, endSetSample);
+
+				// Create PipeData object containing the result tuple and pass
+				// it to the pipes-and-filters chain.
+				PipeData pipeData = new PipeData(resultTuple);
+				if (pipesAndFiltersManager != null) {
+					pipesAndFiltersManager.processData(pipeData);
+				} else {
+					throw new CalculatorException(
+							"No PipesAndFilterManager is set. Could not pass "
+									+ "the result to the PipesAndFilterManager.");
+				}
+			} else {
+				throw new CalculatorException(
+						"Could not access the corresponding start ProbeSetSample.");
+			}
+		}
+	}
+
+}
--- src/de/uka/ipd/sdq/probespec/framework/calculator/ResponseTimeCalculator.java
+++ src/de/uka/ipd/sdq/probespec/framework/calculator/ResponseTimeCalculator.java
@@ -9,39 +9,31 @@ import javax.measure.unit.SI;
 
 import de.uka.ipd.sdq.pipesandfilters.framework.CaptureType;
 import de.uka.ipd.sdq.pipesandfilters.framework.MeasurementMetric;
-import de.uka.ipd.sdq.pipesandfilters.framework.PipeData;
 import de.uka.ipd.sdq.pipesandfilters.framework.Scale;
 import de.uka.ipd.sdq.probespec.framework.IMatchRule;
 import de.uka.ipd.sdq.probespec.framework.ProbeSample;
 import de.uka.ipd.sdq.probespec.framework.ProbeSetSample;
-import de.uka.ipd.sdq.probespec.framework.ProbeSetSampleID;
 import de.uka.ipd.sdq.probespec.framework.ProbeType;
 import de.uka.ipd.sdq.probespec.framework.ProbeTypeMatchRule;
 import de.uka.ipd.sdq.probespec.framework.SampleBlackboard;
 import de.uka.ipd.sdq.probespec.framework.exceptions.CalculatorException;
 
 /**
- * This class is a specific Calculator which calculates a time span representing
- * the ResponseTime. It needs two ProbeSets each containing at least a
- * CURRENT_TIME probe.
+ * Calculates a time span representing the response time. It needs two ProbeSets
+ * each containing at least a CURRENT_TIME probe.
  * 
- * @author Faber
+ * @author Faber, Philipp Merkle
  * 
  */
-public class ResponseTimeCalculator extends Calculator {
-
-	private String startProbeSetID;
-	private String endProbeSetID;
+public class ResponseTimeCalculator extends BinaryCalculator {
 
 	private static Vector<MeasurementMetric> concreteMeasurementMetrics;
 
 	/**
-	 * Constructor for the ResponseTimeCalculator. It takes a reference of the
-	 * blackboard and the IDs of the probe set elements taken from the model.
+	 * Default Constructor.
 	 * 
 	 * @param blackboard
-	 *            A reference to the blackboard which this calculator will
-	 *            observe
+	 *            the blackboard this calculator will observe
 	 * @param startProbeSetID
 	 *            ID of the start probe set element from the model
 	 * @param endProbeSetID
@@ -49,104 +41,62 @@ public class ResponseTimeCalculator extends Calculator {
 	 */
 	public ResponseTimeCalculator(SampleBlackboard blackboard,
 			String startProbeSetID, String endProbeSetID) {
-		super(blackboard);
-		this.startProbeSetID = startProbeSetID;
-		this.endProbeSetID = endProbeSetID;
+		super(blackboard, startProbeSetID, endProbeSetID);
 	}
 
 	/**
-	 * This method is called by the update method (Observer pattern) of the
-	 * superclass. The method tries to get the corresponding start probe set
-	 * sample and then extracts the necessary ProbeSamples. Here we make the
-	 * assumption that the start ProbeSetSample always arrives before the end
-	 * ProbeSetSample. Without this assumption all binary calculators would also
-	 * have to try to get the end ProbeSetSample when the start ProbeSetSample
-	 * arrives. Probably this would have a negative effect on the performance.
+	 * Calculates the response time.
 	 * 
-	 * After the extraction it delegates the calculation to the calculate
-	 * method.
-	 * 
-	 * @param pss
-	 *            The last ProbeSetSample which was added to the
-	 *            SampleBlackboard and so triggered this Calculator.
+	 * @param start
+	 *            the ProbeSample of the start ProbeSet (ProbeType.CURRENT_TIME)
+	 * @param end
+	 *            the ProbeSample of the end ProbeSet (ProbeType.CURRENT_TIME)
 	 */
-	@SuppressWarnings("unchecked")
 	@Override
-	protected void execute(ProbeSetSample pss) throws CalculatorException {
-		ProbeSetSample startSetSample;
-		ProbeSetSample endSetSample;
-
-		// For Binary Calculators: Execute only if second ProbeSetSample
-		// arrives. Here we make the assumption that the start ProbeSetSample
-		// always arrives before the end ProbeSetSample. See JavaDoc comment
-		// above.
-		if (endProbeSetID.equals(pss.getProbeSetSampleID().getProbeSetID())) {
-			endSetSample = pss;
-
-			startSetSample = blackboard.getProbeSetSample(new ProbeSetSampleID(
-					startProbeSetID, pss.getProbeSetSampleID().getCtxID()));
-
-			if (startSetSample != null) {
-				ProbeSample<Long, Duration> startProbeValue = null;
-				ProbeSample<Long, Duration> endProbeValue = null;
-
-				IMatchRule[] rules = new IMatchRule[1];
-				rules[0] = new ProbeTypeMatchRule(ProbeType.CURRENT_TIME);
-				Vector<ProbeSample<?, ? extends Quantity>> result = startSetSample
-						.getProbeSamples(rules);
-				if (result != null && result.size() > 0)
-					startProbeValue = (ProbeSample<Long, Duration>) result
-							.get(0);
-
-				rules[0] = new ProbeTypeMatchRule(ProbeType.CURRENT_TIME);
-				result = endSetSample.getProbeSamples(rules);
-				if (result != null && result.size() > 0)
-					endProbeValue = (ProbeSample<Long, Duration>) result.get(0);
-
-				if (startProbeValue != null && endProbeValue != null) {
-					calculate(startProbeValue, endProbeValue);
-				} else {
-					throw new CalculatorException(
-							"Could not access all needed probe samples.");
-				}
-			} else {
-				throw new CalculatorException(
-						"Could not access the corresponding start ProbeSetSample.");
-			}
+	protected Vector<Measure<?, ? extends Quantity>> calculate(
+			ProbeSetSample start, ProbeSetSample end)
+			throws CalculatorException {
+		// Obtain start time sample
+		ProbeSample<Double, Duration> startTimeSample = obtainCurrentTimeProbeSample(start);
+		if (startTimeSample == null) {
+			throw new CalculatorException(
+					"Could not access start probe sample.");
 		}
-	}
 
-	/**
-	 * This method encapsulates the actual logic of this Calculator. It creates
-	 * a PipeData object containing the result tuple and then passes it to the
-	 * pipe and filter chain.
-	 * 
-	 * @param r1
-	 *            The ProbeSample of the start probe set
-	 *            (ProbeType.CURRENT_TIME)
-	 * @param r2
-	 *            The ProbeSample of the end probe set (ProbeType.CURRENT_TIME)
-	 */
-	private void calculate(ProbeSample<Long, Duration> r1,
-			ProbeSample<Long, Duration> r2) throws CalculatorException {
-		
-		// TODO Assert r1.unit == r2. unit!?
-		Measure<Long, Duration> result = Measure.valueOf(r2.getMeasure()
-				.longValue(r1.getMeasure().getUnit())
-				- r1.getMeasure().longValue(r1.getMeasure().getUnit()), r1.getMeasure().getUnit());
+		// Obtain end time sample
+		ProbeSample<Double, Duration> endTimeSample = obtainCurrentTimeProbeSample(end);
+		if (endTimeSample == null) {
+			throw new CalculatorException("Could not access end probe sample.");
+		}
 
+		// Calculate response time
+		double endTime = endTimeSample.getMeasure().doubleValue(
+				startTimeSample.getMeasure().getUnit());
+		double startTime = startTimeSample.getMeasure().doubleValue(
+				startTimeSample.getMeasure().getUnit());
+		double responseTime = endTime - startTime;
+
+		// Create result tuple
+		Measure<Double, Duration> result = Measure.valueOf(responseTime,
+				startTimeSample.getMeasure().getUnit());
 		Vector<Measure<?, ? extends Quantity>> resultTuple = new Vector<Measure<?, ? extends Quantity>>();
-
 		resultTuple.add(result);
 
-		PipeData pd = new PipeData(resultTuple);
+		return resultTuple;
+	}
 
-		if (pipesAndFiltersManager != null) {
-			pipesAndFiltersManager.processData(pd);
-		} else {
-			throw new CalculatorException(
-					"No PipesAndFilterManager is set. Could not pass the result to the PipesAndFilterManager.");
-		}
+	@SuppressWarnings("unchecked")
+	private ProbeSample<Double, Duration> obtainCurrentTimeProbeSample(
+			ProbeSetSample probeSetSample) {
+		IMatchRule[] rules = new IMatchRule[1];
+		rules[0] = new ProbeTypeMatchRule(ProbeType.CURRENT_TIME);
+		Vector<ProbeSample<?, ? extends Quantity>> result = probeSetSample
+				.getProbeSamples(rules);
+
+		if (result != null && result.size() > 0)
+			return (ProbeSample<Double, Duration>) result.get(0);
+
+		return null;
 	}
 
 	/**
