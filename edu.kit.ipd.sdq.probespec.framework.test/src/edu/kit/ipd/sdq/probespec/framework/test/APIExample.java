3f127b176b09b9651835fe2ccbc80ffca5cfdbd8
pmerkle <merkle@kit.edu>
2012-11-11 09:27:34 +0000

    latest changes to new ProbeSpec version
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/ProbeSpecification/branches/ProbeSpecification2@17004 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- edu.kit.ipd.sdq.probespec.framework.test/src/edu/kit/ipd/sdq/probespec/framework/test/APIExample.java
+++ edu.kit.ipd.sdq.probespec.framework.test/src/edu/kit/ipd/sdq/probespec/framework/test/APIExample.java
@@ -3,6 +3,7 @@ package edu.kit.ipd.sdq.probespec.framework.test;
 import edu.kit.ipd.sdq.probespec.DerivedIntegerProbe;
 import edu.kit.ipd.sdq.probespec.DoubleProbe;
 import edu.kit.ipd.sdq.probespec.IntegerProbe;
+import edu.kit.ipd.sdq.probespec.framework.ProbeFactory;
 import edu.kit.ipd.sdq.probespec.framework.ProbeSpecContext;
 import edu.kit.ipd.sdq.probespec.framework.calculators.binary.example.DifferenceCalculator;
 import edu.kit.ipd.sdq.probespec.framework.calculators.unary.example.PlusOneCalculator;
@@ -12,26 +13,33 @@ public class APIExample {
     public static void main(String[] args) {
         // initialize ProbeSpec
         ProbeSpecContext ctx = new ProbeSpecContext();
-
+        
         // create some basic probes
-        IntegerProbe firstProbe = ctx.getProbeFactory().createIntegerProbe("firstBasicProbe");
-        DoubleProbe secondProbe = ctx.getProbeFactory().createDoubleProbe("secondBasicProbe");
-        IntegerProbe thirdProbe = ctx.getProbeFactory().createIntegerProbe("thirdBasicProbe");
+        IntegerProbe firstProbe = ProbeFactory.createIntegerProbe("firstBasicProbe");
+        DoubleProbe secondProbe = ProbeFactory.createDoubleProbe("secondBasicProbe");
+        IntegerProbe thirdProbe = ProbeFactory.createIntegerProbe("thirdBasicProbe");
 
         // create some derived probes (without binding them to a calculator for the moment)
-        DerivedIntegerProbe derivedProbe = ctx.getProbeFactory().createDerivedIntegerProbe("calculatePlusOneProbe");
-        DerivedIntegerProbe derivedProbe2 = ctx.getProbeFactory().createDerivedIntegerProbe("calculateDifferenceProbe");
+        DerivedIntegerProbe derivedProbe = ProbeFactory.createDerivedIntegerProbe("plusOneProbe");
+        DerivedIntegerProbe derivedProbe2 = ProbeFactory.createDerivedIntegerProbe("differenceProbe");
 
         // now bind each derived probe to exactly one calculator
         ctx.bind(derivedProbe, new PlusOneCalculator(ctx), thirdProbe);
-        ctx.bind(derivedProbe2, new DifferenceCalculator(ctx), derivedProbe, thirdProbe);
+        
+        DifferenceCalculator c = new DifferenceCalculator(ctx);
+        ctx.bindCalculatorInput(c, derivedProbe, thirdProbe);
+        ctx.bindCalculatorOutput(c, derivedProbe2);
 
         // generate some dummy measurements for demonstration purposes
         for (int i = 0; i < 3; i++) {
-            ctx.getBlackboard().addMeasurement(i, firstProbe);
+            ctx.addIntegerMeasurement(i, firstProbe);
         }
-        ctx.getBlackboard().addMeasurement(5.0, secondProbe);
-        ctx.getBlackboard().addMeasurement(5, thirdProbe);
+        ctx.addDoubleMeasurement(5.0, secondProbe);
+        ctx.addIntegerMeasurement(5, thirdProbe);
+        
+        System.out.println(ctx.getBlackboard().getRegion(Integer.class).getLatestMeasurement(thirdProbe));
+        
+        //TODO: support probe.addMeasurement() ?
     }
 
 }
