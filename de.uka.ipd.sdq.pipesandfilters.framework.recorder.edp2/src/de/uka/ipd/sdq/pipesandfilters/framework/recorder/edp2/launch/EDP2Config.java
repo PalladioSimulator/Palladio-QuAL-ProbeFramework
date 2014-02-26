589516a8c85ac4bfc34841584f1d92ec3cd2a716
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-02-26 16:08:15 +0000

    Fixed a typecast exception (the configuration returns a String, not an integer)
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/ProbeSpecification/trunk@20912 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.uka.ipd.sdq.pipesandfilters.framework.recorder.edp2/src/de/uka/ipd/sdq/pipesandfilters/framework/recorder/edp2/launch/EDP2Config.java
+++ de.uka.ipd.sdq.pipesandfilters.framework.recorder.edp2/src/de/uka/ipd/sdq/pipesandfilters/framework/recorder/edp2/launch/EDP2Config.java
@@ -9,17 +9,17 @@ public class EDP2Config implements IRecorderConfiguration, Serializable {
 
     private static final long serialVersionUID = 1L;
     
-    private long repositoryID;
+    private String repositoryID;
     
     public static final String REPOSITORY_ID = "EDP2RepositoryID";
     public static final String PERSISTENCE_RECORDER_ID = "EDP2RecorderID";
     
     public void setConfiguration(Map<String,Object> configuration) {
-        this.repositoryID = (Integer)configuration.get(
+        this.repositoryID = (String) configuration.get(
                 REPOSITORY_ID); 
     }
     
-    public long getRepositoryID() {
+    public String getRepositoryID() {
         return this.repositoryID;
     }
 
