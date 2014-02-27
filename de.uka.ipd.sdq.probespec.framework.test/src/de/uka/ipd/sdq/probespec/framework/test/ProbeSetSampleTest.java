d8418d7902ab264a83484342143104b49f4f4ec7
pmerkle <merkle@kit.edu>
2014-02-27 16:11:43 +0000

    fixed test cases broken due to refactorings a while ago
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/ProbeSpecification/trunk@20951 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.uka.ipd.sdq.probespec.framework.test/src/de/uka/ipd/sdq/probespec/framework/test/ProbeSetSampleTest.java
+++ de.uka.ipd.sdq.probespec.framework.test/src/de/uka/ipd/sdq/probespec/framework/test/ProbeSetSampleTest.java
@@ -5,54 +5,55 @@ import java.util.Vector;
 import javax.measure.quantity.Quantity;
 
 import junit.framework.TestCase;
-import de.uka.ipd.sdq.probespec.framework.matching.IMatchRule;
 import de.uka.ipd.sdq.probespec.framework.ProbeSample;
 import de.uka.ipd.sdq.probespec.framework.ProbeSetSample;
 import de.uka.ipd.sdq.probespec.framework.ProbeType;
-import de.uka.ipd.sdq.probespec.framework.matching.ProbeTypeMatchRule;
 import de.uka.ipd.sdq.probespec.framework.RequestContext;
-import de.uka.ipd.sdq.probespec.framework.probes.AProbeStrategyFactory;
+import de.uka.ipd.sdq.probespec.framework.matching.IMatchRule;
+import de.uka.ipd.sdq.probespec.framework.matching.ProbeTypeMatchRule;
+import de.uka.ipd.sdq.probespec.framework.probes.ProbeStrategyRegistry;
 import de.uka.ipd.sdq.probespec.framework.probes.example.ExampleProbeStrategyRegistry;
 import de.uka.ipd.sdq.probespec.framework.probes.example.SimpleSimulationContext;
 
 public class ProbeSetSampleTest extends TestCase {
 
-	private AProbeStrategyFactory factory;
+    private ProbeStrategyRegistry probeStrategyRegistry;
 
-	private SimpleSimulationContext simCtx;
+    private SimpleSimulationContext simCtx;
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
 
-		simCtx = new SimpleSimulationContext();
+        simCtx = new SimpleSimulationContext();
 
-		// Obtain the suitable probeFactory
-		factory = new ExampleProbeStrategyRegistry(simCtx);
-	}
+        // Obtain the suitable probeFactory
+        probeStrategyRegistry = new ExampleProbeStrategyRegistry();
+    }
 
-	@Override
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 
-	public void testProbeSetSample() {
-		simCtx.setSimulatedTime(100);
+    public void testProbeSetSample() {
+        simCtx.setSimulatedTime(100d);
 
-		Vector<ProbeSample<?, ? extends Quantity>> v = new Vector<ProbeSample<?, ? extends Quantity>>();
+        Vector<ProbeSample<?, ? extends Quantity>> v = new Vector<ProbeSample<?, ? extends Quantity>>();
 
-		// Take a sample of the current simulation time
-		v.add(factory.getTakeCurrentTime().takeSample("theProbeID"));
+        // Take a sample of the current simulation time
+        Object measurableEntity = null; // here, we measure the "simulated" time, there is no entity
+        v.add(probeStrategyRegistry.getProbeStrategy(ProbeType.CURRENT_TIME, measurableEntity).takeSample("myProbeID", simCtx));
 
-		// Create a ProbeSetSample
-		RequestContext ctxID = new RequestContext("1");
-		ProbeSetSample pss = new ProbeSetSample(v, ctxID, "x", "y");
+        // Create a ProbeSetSample
+        RequestContext ctxID = new RequestContext("1");
+        Integer probeSetId = 1;
+        ProbeSetSample pss = new ProbeSetSample(v, ctxID, "x", probeSetId);
 
-		// Check, whether the probeSetSample returns the correct value for the
-		// previously measured simulation time
-		IMatchRule[] rules = { new ProbeTypeMatchRule(ProbeType.CURRENT_TIME) };
-		assertEquals(100l, pss.getProbeSamples(rules).get(0).getMeasure()
-				.getValue());
-	}
+        // Check, whether the probeSetSample returns the correct value for the
+        // previously measured simulation time
+        IMatchRule[] rules = { new ProbeTypeMatchRule(ProbeType.CURRENT_TIME) };
+        assertEquals(100d, pss.getProbeSamples(rules).get(0).getMeasure().getValue());
+    }
 
 }
