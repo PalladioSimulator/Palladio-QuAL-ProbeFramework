26e9bc1075be5e693ae5eaa019f03efb6e92cd98
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2014-03-15 07:38:09 +0000

    Updated due to recent probespec changes
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/ProbeSpecification/trunk@21236 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.uka.ipd.sdq.pipesandfilters.framework/src/de/uka/ipd/sdq/pipesandfilters/framework/recorder/launch/RecorderExtensionHelper.java
+++ de.uka.ipd.sdq.pipesandfilters.framework/src/de/uka/ipd/sdq/pipesandfilters/framework/recorder/launch/RecorderExtensionHelper.java
@@ -10,128 +10,128 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
 
 import de.uka.ipd.sdq.pipesandfilters.framework.IMetaDataInitFactory;
-import de.uka.ipd.sdq.pipesandfilters.framework.recorder.IRawWriteStrategy;
+import de.uka.ipd.sdq.pipesandfilters.framework.recorder.Recorder;
 
 public class RecorderExtensionHelper {
 
-	public static String[] getRecorderNames() throws CoreException {
-		List<IExtension> recorderExtensions = loadExtensions("de.uka.ipd.sdq.pipesandfilters.framework.recorder");
-		List<String> names = new ArrayList<String>();
-		for (IExtension extension : recorderExtensions) {
-			IConfigurationElement e = obtainConfigurationElement("recorder",
-					extension);
-			if (e != null) {
-				names.add(e.getAttribute("name"));
-			}
-		}
-		return names.toArray(new String[names.size()]);
-	}
+    public static String[] getRecorderNames() throws CoreException {
+        final List<IExtension> recorderExtensions = loadExtensions("de.uka.ipd.sdq.pipesandfilters.framework.recorder");
+        final List<String> names = new ArrayList<String>();
+        for (final IExtension extension : recorderExtensions) {
+            final IConfigurationElement e = obtainConfigurationElement("recorder",
+                    extension);
+            if (e != null) {
+                names.add(e.getAttribute("name"));
+            }
+        }
+        return names.toArray(new String[names.size()]);
+    }
 
-	public static ILaunchConfigurationTab[] getLaunchConfigTabs()
-			throws CoreException {
-		List<IExtension> recorderExtensions = loadExtensions("de.uka.ipd.sdq.pipesandfilters.framework.recorder");
-		List<ILaunchConfigurationTab> tabList = new ArrayList<ILaunchConfigurationTab>();
-		for (IExtension extension : recorderExtensions) {
-			IConfigurationElement e = obtainConfigurationElement("recorder",
-					extension);
-			if (e != null) {
-				tabList.add((ILaunchConfigurationTab) e
-						.createExecutableExtension("launchConfigTab"));
-			}
-		}
-		return tabList.toArray(new ILaunchConfigurationTab[tabList.size()]);
-	}
+    public static ILaunchConfigurationTab[] getLaunchConfigTabs()
+            throws CoreException {
+        final List<IExtension> recorderExtensions = loadExtensions("de.uka.ipd.sdq.pipesandfilters.framework.recorder");
+        final List<ILaunchConfigurationTab> tabList = new ArrayList<ILaunchConfigurationTab>();
+        for (final IExtension extension : recorderExtensions) {
+            final IConfigurationElement e = obtainConfigurationElement("recorder",
+                    extension);
+            if (e != null) {
+                tabList.add((ILaunchConfigurationTab) e
+                        .createExecutableExtension("launchConfigTab"));
+            }
+        }
+        return tabList.toArray(new ILaunchConfigurationTab[tabList.size()]);
+    }
 
-	public static String getExtensionIdentifierForName(String recorderName)
-			throws CoreException {
-		List<IExtension> recorderExtensions = loadExtensions("de.uka.ipd.sdq.pipesandfilters.framework.recorder");
-		for (IExtension extension : recorderExtensions) {
-			IConfigurationElement e = obtainConfigurationElement("recorder",
-					extension);
-			if (e != null && e.getAttribute("name").equals(recorderName)) {
-				return extension.getUniqueIdentifier();
-			}
-		}
-		return null;
-	}
+    public static String getExtensionIdentifierForName(final String recorderName)
+            throws CoreException {
+        final List<IExtension> recorderExtensions = loadExtensions("de.uka.ipd.sdq.pipesandfilters.framework.recorder");
+        for (final IExtension extension : recorderExtensions) {
+            final IConfigurationElement e = obtainConfigurationElement("recorder",
+                    extension);
+            if (e != null && e.getAttribute("name").equals(recorderName)) {
+                return extension.getUniqueIdentifier();
+            }
+        }
+        return null;
+    }
 
-	public static IRecorderConfiguration getRecorderConfigForName(
-			String recorderName) throws CoreException {
-		List<IExtension> recorderExtensions = loadExtensions("de.uka.ipd.sdq.pipesandfilters.framework.recorder");
-		for (IExtension extension : recorderExtensions) {
-			IConfigurationElement e = obtainConfigurationElement("recorder",
-					extension);
-			if (e != null && e.getAttribute("name").equals(recorderName)) {
-				Object config = e.createExecutableExtension("configuration");
-				if (config != null) {
-					return (IRecorderConfiguration) config;
-				}
-			}
-		}
-		return null;
-	}
+    public static IRecorderConfiguration getRecorderConfigForName(
+            final String recorderName) throws CoreException {
+        final List<IExtension> recorderExtensions = loadExtensions("de.uka.ipd.sdq.pipesandfilters.framework.recorder");
+        for (final IExtension extension : recorderExtensions) {
+            final IConfigurationElement e = obtainConfigurationElement("recorder",
+                    extension);
+            if (e != null && e.getAttribute("name").equals(recorderName)) {
+                final Object config = e.createExecutableExtension("configuration");
+                if (config != null) {
+                    return (IRecorderConfiguration) config;
+                }
+            }
+        }
+        return null;
+    }
 
-    public static IRawWriteStrategy instantiateWriteStrategyForRecorder(String recorderName) {
+    public static Recorder instantiateWriteStrategyForRecorder(final String recorderName) {
         try {
-            return (IRawWriteStrategy) instantiateExecutableExtension(recorderName, "writeStrategy");
-        } catch (CoreException e) {
+            return (Recorder) instantiateExecutableExtension(recorderName, "writeStrategy");
+        } catch (final CoreException e) {
             throw new RuntimeException("Could not instantiate write strategy for recorder named " + recorderName);
         }
     }
-	
-    public static IMetaDataInitFactory instantiateMetaDataInitFactoryForRecorder(String recorderName) {
+
+    public static IMetaDataInitFactory instantiateMetaDataInitFactoryForRecorder(final String recorderName) {
         try {
             return (IMetaDataInitFactory) instantiateExecutableExtension(recorderName, "metaDataInitFactory");
-        } catch (CoreException e) {
+        } catch (final CoreException e) {
             throw new RuntimeException("Could not instantiate MetaDataInit factory for recorder named " + recorderName);
         }
     }
-	
-	private static Object instantiateExecutableExtension(String recorderName, String attributeName)
-	        throws CoreException {
-	    List<IExtension> recorderExtensions = loadExtensions("de.uka.ipd.sdq.pipesandfilters.framework.recorder");
-        for (IExtension extension : recorderExtensions) {            
-            IConfigurationElement e = obtainConfigurationElement("recorder",
-                    extension);            
+
+    private static Object instantiateExecutableExtension(final String recorderName, final String attributeName)
+            throws CoreException {
+        final List<IExtension> recorderExtensions = loadExtensions("de.uka.ipd.sdq.pipesandfilters.framework.recorder");
+        for (final IExtension extension : recorderExtensions) {
+            final IConfigurationElement e = obtainConfigurationElement("recorder",
+                    extension);
             if (e != null && e.getAttribute("name").equals(recorderName)) {
                 return e.createExecutableExtension(attributeName);
             }
         }
         return null;
-	}
+    }
 
-	public static String getNameForExtensionIdentifier(String extensionID)
-			throws CoreException {
-		IExtension ext = Platform.getExtensionRegistry().getExtensionPoint(
-				"de.uka.ipd.sdq.pipesandfilters.framework.recorder")
-				.getExtension(extensionID);
-		if (ext != null) {
-			IConfigurationElement e = obtainConfigurationElement("recorder",
-					ext);
-			return e.getAttribute("name");
-		}
-		return null;
-	}
+    public static String getNameForExtensionIdentifier(final String extensionID)
+            throws CoreException {
+        final IExtension ext = Platform.getExtensionRegistry().getExtensionPoint(
+                "de.uka.ipd.sdq.pipesandfilters.framework.recorder")
+                .getExtension(extensionID);
+        if (ext != null) {
+            final IConfigurationElement e = obtainConfigurationElement("recorder",
+                    ext);
+            return e.getAttribute("name");
+        }
+        return null;
+    }
 
-	private static IConfigurationElement obtainConfigurationElement(
-			String elementName, IExtension extension) throws CoreException {
-		IConfigurationElement[] elements = extension.getConfigurationElements();
-		for (IConfigurationElement element : elements) {
-			if (element.getName().equals(elementName)) {
-				return element;
-			}
-		}
-		return null;
-	}
+    private static IConfigurationElement obtainConfigurationElement(
+            final String elementName, final IExtension extension) throws CoreException {
+        final IConfigurationElement[] elements = extension.getConfigurationElements();
+        for (final IConfigurationElement element : elements) {
+            if (element.getName().equals(elementName)) {
+                return element;
+            }
+        }
+        return null;
+    }
 
-	private static List<IExtension> loadExtensions(String extensionPointID) {
-		IExtension[] exts = Platform.getExtensionRegistry().getExtensionPoint(
-				extensionPointID).getExtensions();
-		List<IExtension> results = new ArrayList<IExtension>();
-		for (IExtension extension : exts) {
-			results.add(extension);
-		}
-		return results;
-	}
+    private static List<IExtension> loadExtensions(final String extensionPointID) {
+        final IExtension[] exts = Platform.getExtensionRegistry().getExtensionPoint(
+                extensionPointID).getExtensions();
+        final List<IExtension> results = new ArrayList<IExtension>();
+        for (final IExtension extension : exts) {
+            results.add(extension);
+        }
+        return results;
+    }
 
 }
