d90d34477b38531b0af6cb52eca072e35b6a5a6a
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-05-05 07:37:13 +0000

    JavaDoc for the Probe Framework context
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/ProbeFramework/trunk@22225 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.probeframework/src/org/palladiosimulator/probeframework/package-info.java
+++ org.palladiosimulator.probeframework/src/org/palladiosimulator/probeframework/package-info.java
@@ -2,8 +2,12 @@
  * This package contains all classes relevant within the Probe Framework. 
  * The two central concepts are probes and calculators.
  * 
+ * A good overview is provided in the Performance Lab documentation
+ * (<code>org.palladiosimulator.perflab.docs</code>).
+ * 
  * TODO Fix inconsistency: plural for probe, singular for calculator packages. [Lehrig]
  * 
+ * @see org.palladiosimulator.perflab.docs
  * @see org.palladiosimulator.probeframework.probes
  * @see org.palladiosimulator.probeframework.calculator
  * 
