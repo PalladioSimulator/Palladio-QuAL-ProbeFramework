ad948038da03a5311c5179a62ee9d86e51f1710d
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-05-05 12:34:36 +0000

    JavaDoc for event probes.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/ProbeFramework/trunk@22234 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.probeframework/src/org/palladiosimulator/probeframework/probes/BasicEventProbe.java
+++ org.palladiosimulator.probeframework/src/org/palladiosimulator/probeframework/probes/BasicEventProbe.java
@@ -9,12 +9,50 @@ import org.palladiosimulator.metricspec.BaseMetricDescription;
 import org.palladiosimulator.probeframework.measurement.ProbeMeasurement;
 import org.palladiosimulator.probeframework.measurement.RequestContext;
 
+/**
+ * Basic event probes refer to exactly one base metric description, thus, specializing the general
+ * event probe.
+ * 
+ * Moreover, basic event probes provide a generic <code>notify</code> method. This method allows
+ * subclasses to pass an event measurement to it when they receive an event. Subsequently,
+ * <code>notify</code> can generically inform registered probe listeners about a newly available
+ * probe measurement. For characterizing the measurement itself, basic event probes refer to V as
+ * the value of the measure to Q as their quantity.
+ * 
+ * Note that this class still leaves the implementation of the abstract method
+ * <code>registerListener</code> to its subclasses.
+ * 
+ * @author Steffen Becker, Sebastian Lehrig
+ * 
+ * @param <EventSourceType>
+ *            The type of the event source (as needed by the superclass).
+ * @param <V>
+ *            The value type of the basic measure (as needed by the superclass).
+ * @param <Q>
+ *            The quantity type of the basic measure (as needed by the superclass).
+ */
 public abstract class BasicEventProbe<EventSourceType, V, Q extends Quantity> extends EventProbe<EventSourceType> {
 
-    public BasicEventProbe(final EventSourceType eventSource, final BaseMetricDescription metric) {
-        super(eventSource, metric);
+    /**
+     * Default constructor.
+     * 
+     * @param eventSource
+     *            The event source as needed by the superclass.
+     * @param metricDescription
+     *            The metric description as needed by the superclass.
+     */
+    public BasicEventProbe(final EventSourceType eventSource, final BaseMetricDescription metricDescription) {
+        super(eventSource, metricDescription);
     }
 
+    /**
+     * Allows subclasses to pass an event measurement to it when they receive an event.
+     * Subsequently, <code>notify</code> can generically inform registered probe listeners about a
+     * newly available probe measurement.
+     * 
+     * @param eventMeasure
+     *            The measurement received from the event.
+     */
     protected void notify(final Measure<V, Q> eventMeasure) {
         final Measurement basicMeasurement = new BasicMeasurement<V, Q>(eventMeasure, this.getMetricDesciption());
         final ProbeMeasurement newMeasurement = new ProbeMeasurement(basicMeasurement, this,
--- org.palladiosimulator.probeframework/src/org/palladiosimulator/probeframework/probes/EventProbe.java
+++ org.palladiosimulator.probeframework/src/org/palladiosimulator/probeframework/probes/EventProbe.java
@@ -2,15 +2,38 @@ package org.palladiosimulator.probeframework.probes;
 
 import org.palladiosimulator.metricspec.MetricDescription;
 
+/**
+ * Event probes measure as soon as an event is emitted for which they are registered. Therefore,
+ * they explicitly refer to an <code>eventSource</code> object. Subclasses have to realize the
+ * registration to this object by implementing the template method <code>registerListener</code>.
+ * 
+ * @author Steffen Becker, Sebastian Lehrig
+ * 
+ * @param <EventSourceType>
+ *            The type of the event source.
+ */
 public abstract class EventProbe<EventSourceType> extends Probe {
 
+    /** The event source. */
     protected final EventSourceType eventSource;
 
+    /**
+     * Default constructor. Expects a suitable event source object (main characteristic of this type
+     * of probe).
+     * 
+     * @param eventSource
+     *            The event source object.
+     * @param metricDesciption
+     *            The metric description as needed by the superclass.
+     */
     public EventProbe(final EventSourceType eventSource, final MetricDescription metricDesciption) {
         super(metricDesciption);
         this.eventSource = eventSource;
         registerListener();
     }
 
+    /**
+     * Template method for registering observers to the event source.
+     */
     protected abstract void registerListener();
 }
