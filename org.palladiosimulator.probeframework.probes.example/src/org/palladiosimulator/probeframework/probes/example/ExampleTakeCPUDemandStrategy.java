6784c47c20008243deb47a9462638c81dbefcc89
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-05-05 12:34:51 +0000

    JavaDoc for event probes.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/ProbeFramework/trunk@22235 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.probeframework.probes.example/src/org/palladiosimulator/probeframework/probes/example/ExampleTakeCPUDemandStrategy.java
+++ org.palladiosimulator.probeframework.probes.example/src/org/palladiosimulator/probeframework/probes/example/ExampleTakeCPUDemandStrategy.java
@@ -10,18 +10,44 @@ import javax.measure.unit.SI;
 import org.palladiosimulator.metricspec.constants.MetricDescriptionConstants;
 import org.palladiosimulator.probeframework.probes.BasicEventProbe;
 
+/**
+ * Measures a resource demand metric (in {@link SI#SECOND}) by taking the demand emitted from an
+ * active resource (event source), e.g., a CPU. This class uses <code>ASimpleActiveResource</code>
+ * as an example active resource.
+ * 
+ * @author Steffen Becker, Sebastian Lehrig
+ */
 public class ExampleTakeCPUDemandStrategy extends BasicEventProbe<ASimpleActiveResource, Double, Duration> implements
         Observer {
 
-    public ExampleTakeCPUDemandStrategy(final ASimpleActiveResource myResource) {
-        super(myResource, MetricDescriptionConstants.RESOURCE_DEMAND_METRIC);
+    /**
+     * Default constructor.
+     * 
+     * @param activeResource
+     *            The event source is an active resource (e.g., a CPU), thus, able to emit demand
+     *            events.
+     */
+    public ExampleTakeCPUDemandStrategy(final ASimpleActiveResource activeResource) {
+        super(activeResource, MetricDescriptionConstants.RESOURCE_DEMAND_METRIC);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void registerListener() {
         this.eventSource.addObserver(this);
     }
 
+    /**
+     * Receives a new demand event from the active resource (event source). Afterwards, notifies all
+     * observers about this new measurement.
+     * 
+     * @param eventSource
+     *            The observed event source.
+     * @param event
+     *            The emitted event, including the demand measurement.
+     */
     @Override
     public void update(final Observable eventSource, final Object event) {
         final double demand = (Double) event;
--- org.palladiosimulator.probeframework.probes.example/src/org/palladiosimulator/probeframework/probes/example/SimpleMutEx.java
+++ org.palladiosimulator.probeframework.probes.example/src/org/palladiosimulator/probeframework/probes/example/SimpleMutEx.java
@@ -1,7 +1,16 @@
 package org.palladiosimulator.probeframework.probes.example;
 
+/**
+ * Represents a mutex for demonstration purposes. Such a mutex is a special case of a passive
+ * resource with size 1.
+ * 
+ * @author pmerkle, Sebastian Lehrig
+ */
 public class SimpleMutEx extends ASimplePassiveResource {
 
+    /**
+     * Default constructor. A mutex is a passive resource of size 1.
+     */
     public SimpleMutEx() {
         super(1);
     }
