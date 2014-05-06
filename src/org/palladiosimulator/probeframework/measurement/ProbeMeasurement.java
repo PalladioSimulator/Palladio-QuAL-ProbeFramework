6690f806a958674f6d4b5617b74e123f41d42b81
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-05-06 07:14:32 +0000

    JavaDoc for probe measurements.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/ProbeFramework/trunk/org.palladiosimulator.probeframework@22264 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- src/org/palladiosimulator/probeframework/measurement/ProbeMeasurement.java
+++ src/org/palladiosimulator/probeframework/measurement/ProbeMeasurement.java
@@ -3,17 +3,38 @@ package org.palladiosimulator.probeframework.measurement;
 import org.palladiosimulator.measurementspec.Measurement;
 import org.palladiosimulator.probeframework.probes.Probe;
 
+/**
+ * Data class representing a ({@link Measurement}, ProbeAndRequestContext, String)-tuple, where the
+ * String element refers to a measured entity. Therefore, concrete data objects can store a
+ * measurement coming from a given ProbeAndRequestContext that probes the given measured entity.
+ * 
+ * @author Steffen Becker, Sebastian Lehrig
+ */
 public class ProbeMeasurement {
+
+    /** The measurement to be stored. */
     private final Measurement measurement;
+
+    /** The probe and the request context. */
     private final ProbeAndRequestContext probeAndContext;
+
+    /** The measured entity. */
     private final String measuredEntity;
 
     /**
+     * Default constructor. Constructs the object tuple.
+     * 
      * @param measurement
-     * @param probeAndContext
+     *            The measurement to be stored.
+     * @param probe
+     *            The referred probe.
+     * @param requestContext
+     *            The referred request context.
+     * @param measuredEntity
+     *            A String representing the probed entity.
      */
-    public ProbeMeasurement(final Measurement measurement, final Probe probe,
-            final RequestContext requestContext, final String measuredEntity) {
+    public ProbeMeasurement(final Measurement measurement, final Probe probe, final RequestContext requestContext,
+            final String measuredEntity) {
         super();
         this.measurement = measurement;
         this.probeAndContext = new ProbeAndRequestContext(probe, requestContext);
@@ -21,21 +42,27 @@ public class ProbeMeasurement {
     }
 
     /**
-     * @return the measurement
+     * Getter for the measurement.
+     * 
+     * @return The measurement.
      */
     public final Measurement getMeasurement() {
         return measurement;
     }
 
     /**
-     * @return the probeAndContext
+     * Getter for the probe and request context.
+     * 
+     * @return The probeAndContext.
      */
     public final ProbeAndRequestContext getProbeAndContext() {
         return probeAndContext;
     }
 
     /**
-     * @return the measuredEntity
+     * Getter for the measured entity String.
+     * 
+     * @return The measuredEntity.
      */
     public final String getMeasuredEntity() {
         return measuredEntity;
--- src/org/palladiosimulator/probeframework/measurement/package-info.java
+++ src/org/palladiosimulator/probeframework/measurement/package-info.java
@@ -1,5 +1,7 @@
 /**
- * 
+ * This package contains data classes encapsulating probe-related measurements. Therefore,
+ * concepts such as the request context of a probe, its measurement, and the probed entity
+ * are covered.
  * 
  * @author Steffen Becker, Sebastian Lehrig
  */
